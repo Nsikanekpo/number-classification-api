e0e743f664bafccfea518d8eaf290e6198dbd975
Nsikan <ekponsikan@gmail.com>
2025-02-06 23:59:37 +0100

    modified classify/urls.py

--- classify/urls.py
+++ classify/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import NumberClassificationView
 
 urlpatterns = [
-    path("classify/<int:number/>", NumberClassificationView.as_view(), name="number-classify"),
+    path("classify/<int:number>/", NumberClassificationView.as_view(), name="number-classify"),
 ]
 
